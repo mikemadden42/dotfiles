f446fdc5dc2803e6e989baa05dda4c37cdcc6b27
Michael Madden <mikemadden42@icloud.com>
2025-08-04 21:40:44 -0500

    Initial config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,36 @@
+# Remove greeting
+set -g fish_greeting
+
+# Vi mode
+fish_vi_key_bindings
+
+# Environment variables
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
+set -gx QT_QPA_PLATFORM xcb
+
+# PATH configuration
+set -gx PATH $HOME/bin $GOPATH/bin $HOME/rusty-tools $HOME/.cargo/bin $HOME/.local/bin $PATH
+
+# Aliases
+alias vim='nvim'
+alias vi='nvim'
+alias view='nvim -R'
+alias vimdiff='nvim -d'
+
+# Gruvbox Dark colors
+set -g fish_color_normal normal
+set -g fish_color_command green
+set -g fish_color_quote yellow
+set -g fish_color_redirection cyan
+set -g fish_color_end green
+set -g fish_color_error red
+set -g fish_color_param blue
+set -g fish_color_comment grey
+set -g fish_color_match --background=brblue
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_operator cyan
+set -g fish_color_escape yellow
+set -g fish_color_autosuggestion brblack
